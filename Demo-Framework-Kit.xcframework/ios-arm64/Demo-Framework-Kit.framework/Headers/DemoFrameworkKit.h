94d35a5fc6277a2d3add606a3172ad32c05b40b0
Prit Kothadiya <prithkothadiya@Aurionpros-MacBook-Pro.local>
2023-07-13 10:47:32 +0530

    Second commit

---
+++ Demo-Framework-Kit.xcframework/ios-arm64/Demo-Framework-Kit.framework/Headers/DemoFrameworkKit.h
@@ -0,0 +1,18 @@
+//
+//  DemoFrameworkKit.h
+//  DemoFrameworkKit
+//
+//  Created by Prit Kothadiya on 13/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoFrameworkKit.
+FOUNDATION_EXPORT double DemoFrameworkKitVersionNumber;
+
+//! Project version string for DemoFrameworkKit.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFrameworkKit/PublicHeader.h>
+
+
